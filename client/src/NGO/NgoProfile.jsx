ba1304eb9d0951e627dada368e1fef370eeca145
Pravin kumar <pravinkumarnattu@gmail.com>
2026-08-09 00:50:55 +0530

    Ngo profile added

--- client/src/NGO/NgoProfile.jsx
+++ client/src/NGO/NgoProfile.jsx
@@ -1,5 +1,74 @@
+import { useEffect, useState } from "react";
+import api from "../api/axiosInstance";
+import "./NgoProfile.css";
 
+const views = {
+  initial: "INITIAL",
+  success: "SUCCESS",
+  failure: "FAILURE",
+  loading: "LOADING",
+};
 
-const NgoProfile = () => <div>Profile</div>;
+const NgoProfile = () => {
+  const [errMsg, setErrMsg] = useState("");
+  const [currView, setCurrView] = useState(views.initial);
+  const [userDetails, setuserDetails] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setCurrView(views.loading);
+        const response = await api.get("/auth/me");
+        setuserDetails(response?.data);
+        setCurrView(views.success);
+      } catch (err) {
+        setCurrView(views.failure);
+        setErrMsg(err?.response?.data?.message);
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const loadingView = () => <div className="loading-view">Loading...</div>;
+
+  const profile = () => (
+    <div className="profile-container">
+      <h1>My Profile</h1>
+      <p>
+        <strong>Email:</strong> {userDetails?.email}
+      </p>
+      <p>
+        <strong>Phone:</strong> {userDetails?.phone}
+      </p>
+      <p>
+        <strong>Organization Name:</strong> {userDetails?.ngo?.organizationName}
+      </p>
+      <p>
+        <strong>Registration Number:</strong> {userDetails?.ngo?.ngoRegistrationNumber}
+      </p>
+      <p>
+        <strong>Address:</strong> {userDetails?.ngo?.organizationAddress}
+      </p>
+    </div>
+  );
+
+  const failureView = () => <div className="failure-view">{errMsg}</div>;
+
+  const render = () => {
+    switch (currView) {
+      case views.loading:
+        return loadingView();
+      case views.success:
+        return profile();
+      case views.failure:
+        return failureView();
+      default:
+        return <></>;
+    }
+  };
+
+  return <>{render()}</>;
+};
 
 export default NgoProfile;
